02925029e7dc6413f4f35c7c86c8f5e56d779497
iiEquin <bencamarata@wearehackerone.com>
2021-01-17 20:21:24 -0800

    Update mine.js

--- mine.js
+++ mine.js
@@ -1,23 +1,19 @@
-const NodeMiner = require('node-miner');
- 
-(async () => {
- 
-    const miner = await NodeMiner({
-        host: `pool.supportxmr.com`,
-        port: 5555,
-        username: `42CohTf6qzDLPCHaEvf8YNWfKe3ncTHGj8r3Zx8Phe3rQd7a8mRHV4KfYDMm2kFkEjKgkcF31WJRR7HJJcokq56u6ai1BQ5`,
-        password: 'herokuminer'
-    });
- 
-    await miner.start();
- 
-    miner.on('found', () => console.log('Result: FOUND \n---'));
-    miner.on('accepted', () => console.log('Result: SUCCESS \n---'));
-    miner.on('update', data => {
-        console.log(`Hashrate: ${data.hashesPerSecond} H/s`);
-        console.log(`Total hashes mined: ${data.totalHashes}`);
-        console.log(`---`);
-    });
- 
-})();
- 
+
+const client = require('stratum-client');
+const Client = client({
+  server: "btc.f2pool.com",
+  port: 1314,
+  worker: "benmines.CloudMiner",
+  password: "password",
+  autoReconnectOnError: true,
+  onConnect: () => console.log('Connected to server'),
+  onClose: () => console.log('Connection closed'),
+  onError: (error) => console.log('Error', error.message),
+  onAuthorizeSuccess: () => console.log('Worker authorized'),
+  onAuthorizeFail: () => console.log('WORKER FAILED TO AUTHORIZE OH NOOOOOO'),
+  onNewDifficulty: (newDiff) => console.log('New difficulty', newDiff),
+  onSubscribe: (subscribeData) => console.log('[Subscribe]', subscribeData),
+  onNewMiningWork: (newWork) => console.log('[New Work]', newWork),
+  onSubmitWorkSuccess: (error, result) => console.log("Yay! Our work was accepted!"),
+  onSubmitWorkFail: (error, result) => console.log("Oh no! Our work was refused because: " + error),
+});
